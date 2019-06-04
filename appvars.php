0d319bcb4e0c93e4f8ed3aba7d7c4893e4019c88
benjamin9625 <51328739+benjamin9625@users.noreply.github.com>
2019-06-04 13:33:50 +0200

    Add files via upload

---
+++ appvars.php
@@ -0,0 +1,6 @@
+<?php
+  // Define application constants
+  define('GW_UPLOADPATH', 'images/');
+  define('PH_UPLOADPATH', 'C:\inetpub\wwwroot\Web\images\\');
+  define('GW_MAXFILESIZE', 3200000);      // 32 KB
+?>
